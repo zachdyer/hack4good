e8b69f9219bc0c13b33f198fe91aefcf5458e378
Zach Dyer <zach.dyer@zachdyerdesign.com>
2018-11-03 19:35:26 +0000

    chat view

--- front-end/src/Chat.jsx
+++ front-end/src/Chat.jsx
@@ -1,12 +1,30 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { Widget, addResponseMessage } from 'react-chat-widget';
 
-export default class Chat extends Component{
+import 'react-chat-widget/lib/styles.css';
+import './Chat.css';
 
-    render() {
-        return(
-            <div>
-                Test
-            </div>
-        );
-    }
+class App extends Component {
+    componentDidMount() {
+        addResponseMessage("Welcome to this awesome chat!");
+      }
+  
+    handleNewUserMessage = (newMessage) => {
+        console.log(`New message incoming! ${newMessage}`);
+        // Now send the message throught the backend API
+        addResponseMessage("How can I help you?");
+      }
+  render() {
+    return (
+      <div className="App">
+        <Widget 
+            handleNewUserMessage={this.handleNewUserMessage}
+            title="Chat Window"
+            subtitle="We are connecting you to an angel now..."
+        />
+      </div>
+    );
+  }
 }
+
+export default App;
