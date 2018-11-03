32d1a238751a45ccc48e2f3b31da643eeeeb2617
Zach Dyer <zach.dyer@zachdyerdesign.com>
2018-11-03 05:19:38 +0000

    added getuserinfo function

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,32 +7,32 @@ function isLoggedIn() {
     return true;
 }
 
+function getUserInfo() {
+  return {
+    name: "John Doe",
+    email: "john.doe@gmail.com"
+  }
+}
+
 class App extends Component {
   render() {
+    var userInfo = getUserInfo();
     if(isLoggedIn()) {
       console.log("user is logged in");
       return (
-        <div className="App">
+        <div className="App" id="app">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <p>
-              User is logged in.
+              {userInfo.name} is logged in.
             </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
           </header>
         </div>
       );
     } else {
       console.log("user is not logged in");
       return (
-        <div className="App">
+        <div className="App" id="app">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <p>
