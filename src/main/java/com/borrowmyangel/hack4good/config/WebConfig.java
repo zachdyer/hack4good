ba0a335f5a5248642fef94dc89f195d37bc12f62
explodingkittns <bhigh002@drury.edu>
2018-11-03 18:34:06 -0500

    Added XSS origin config test

--- src/main/java/com/borrowmyangel/hack4good/config/WebConfig.java
+++ src/main/java/com/borrowmyangel/hack4good/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.borrowmyangel.hack4good.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -53,5 +54,15 @@ public class WebConfig implements WebMvcConfigurer {
 				.setCachePeriod(3600)
 				.resourceChain(true)
 				.addResolver(new PathResourceResolver());
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
 	}
 }
