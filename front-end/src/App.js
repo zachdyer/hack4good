703dd572b4a2fa7875c5185f49fa61f6f5863771
Zach Dyer <zach.dyer@zachdyerdesign.com>
2018-11-03 03:54:44 +0000

    isLoggedIn function created

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,14 +2,20 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function isLoggedIn() {
+    return true;
+}
+
 class App extends Component {
   render() {
-    return (
+    if(isLoggedIn()) {
+      console.log("user is logged in");
+      return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            User is logged in.
           </p>
           <a
             className="App-link"
@@ -22,6 +28,28 @@ class App extends Component {
         </header>
       </div>
     );
+    } else {
+      console.log("user is not logged in");
+      return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            User is not logged in.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+    }
+    
   }
 }
 
