be0e4f031f7f45dba7c9b0e864129439306dd2d7
Zach Dyer <zach.dyer@zachdyerdesign.com>
2018-11-04 01:20:08 +0000

    Added logo to chat page and redirect

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-// import ReactDOM from 'react-dom'
-// import logo from './logo.svg';
 import './App.css';
 import Login from "./Login";
 import CreateAccount from "./CreateAccount"
--- front-end/src/Chat.jsx
+++ front-end/src/Chat.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 import { Widget, addResponseMessage } from 'react-chat-widget';
-
+import Home from "./App.jsx";
 import 'react-chat-widget/lib/styles.css';
 import './Chat.css';
 
 class App extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        create : false,
+        login : false,
+        immediate : false
+      }
+      this.toggleHome = this.toggleHome.bind(this);
+    }
+    
     componentDidMount() {
         addResponseMessage("Welcome to this awesome chat!");
       }
@@ -14,17 +24,31 @@ class App extends Component {
         // Now send the message throught the backend API
         addResponseMessage("How can I help you?");
       }
-  render() {
-    return (
-      <div className="App">
-        <Widget 
-            handleNewUserMessage={this.handleNewUserMessage}
-            title="Chat Window"
-            subtitle="We are connecting you to an angel now..."
-        />
-      </div>
-    );
-  }
+      
+    toggleHome() {
+        this.setState(
+            state => ({
+                home: true
+            })
+        );
+    }
+    render() {
+      if(this.state.home) {
+        return (<Home />)
+      }
+      return (
+        <div className="container Chat">
+          <a onClick={this.toggleHome}>
+              <img src="logo.png" className="mb-4" alt="" width="100%" />
+          </a>
+          <Widget 
+              handleNewUserMessage={this.handleNewUserMessage}
+              title="Chat Window"
+              subtitle="We are connecting you to an angel now..."
+          />
+        </div>
+      );
+    }
 }
 
 export default App;
